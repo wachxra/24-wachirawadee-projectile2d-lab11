9ab8a6d7008d937f6b13e84042e5f08904131e88
wachxra <wachirawadee.junt@bumail.net>
2025-05-01 21:17:19 +0700

    Mapping and added raycast for target
    - Added script: Projectile2D.cs
    - Mapping to shooter (from Projectile2D.cs)
    - Added raycast for target -> can be use target(crosshair) to mouse click anywhere on BG

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
 {
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("Hit: " + hit.collider.name);
+            }
+        }
+    }
 }
